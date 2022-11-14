17f9d14f65e341971253bf9e1d83bf947e53d1eb
Богдан К <epam.2@mail.ru>
2022-11-14 17:59:38 +0400

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            positiveNumbers();
+        }
+
+       static void positiveNumbers() //метод для подсчёта положительных чисел
+        {
+            List<int> arr = new List<int>();
+            string input = " ";
+
+            Console.Write("Type ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("'q' ");
+            Console.ResetColor();
+            
+            Console.WriteLine("to end number entry");
+
+            for (int i = 0; ; i++ )
+            {
+                input = Console.ReadLine();
+
+                if (input != "q")
+                    arr.Add(Convert.ToInt32(input));
+                else break;
+
+            }
+
+            int count = 0; //счётчик положительных чисел
+            foreach(int i in arr)
+            {
+                if (i > 0) count++;
+            }
+
+
+            Console.WriteLine($"Положительных чисел введено: {count}");
+            
+        }
+    }
+}
